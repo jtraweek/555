1e46e7a1f9cfbc7a6a124a6cdc6ccd7153288541
Maryam Al Mansour <eng.maryam.al.mansour@gmail.com>
2017-10-17 16:38:53 -0400

    Add files via upload

---
+++ Sprint1/user_story_7.py
@@ -0,0 +1,42 @@
+'''
+@author: Maryam
+Created: 10/17/2017
+'''
+
+import GedcomClass
+from datetime import datetime
+
+"""
+   User Story 7: Legit age for indivisual
+   Assuring an indivisual has a date bigger than 0 and less than 150
+   :return: True on valid
+"""
+file = open('./Test GEDCOM Files/JULIE GEDCOM.ged', 'r')
+indivisuals = GedcomClass.read_individuals(file)
+person_istance = indivisuals.get()
+
+def get_age(person_id, individuals):
+    """
+    This function checks for the age of an individual.
+    It either returns the age or 'NA'
+    """
+
+    def age(self):
+        """
+        Built-in age calculation
+        :return: 'NA' on older than 150 or younger than 0
+        """
+        Birthday = person_istance.birt
+        Deathday = person_istance.deat
+        if not Birthday:
+            return 'NA'
+
+        birth = Birthday
+        if not Deathday:
+            death = datetime.today()
+        else:
+            death = Deathday
+        age = death.year - birth.year
+        if age < 150:
+            return age
+        return 'NA'
