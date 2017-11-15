acdf25664a6913d73d3c774261f9b94f40aff1df
Jiazhen Liu <ljzc18@gmail.com>
2017-11-14 21:40:04 -0500

    Fix us39

--- Sprint4/user_story_39.py
+++ Sprint4/user_story_39.py
@@ -1,25 +1,20 @@
-import GedcomClass
 import datetime
 from datetime import timedelta
 
 
-def upcomingAnniversaries(families,individuals):
-    curr_date = datetime.date.today()
+def upcomingAnniversaries(families, individuals):
+    curr_date = datetime.datetime.today()
     after_30days = curr_date + timedelta(days=30)
 
-    spouse_list =[]
+    spouse_list = []
 
-    for family in families:
-        check_family = GedcomClass.get_family(family, families)
-        check_husb = individuals.get(check_family.husb).name
-        check_wife = individuals.get(check_family.wife).name
-        check_marr = check_family.marr
-        marr_day = int(check_marr.day)
-        marr_month = int(check_marr.month)
-        this_year = int(datetime.date.year)
-        this_marr = datetime.date(this_year,marr_month, marr_day)
-        if check_marr != 'NA':
-            if curr_date <= this_marr <= after_30days:
-                spouse_list.append("( "+ check_husb +" "+check_wife+" )")
+    for family in families.values():
+        check_husb = individuals.get(family.husb).name
+        check_wife = individuals.get(family.wife).name
+        marr = family.marr
+        if marr != 'NA':
+            marr = marr.replace(year=curr_date.year)
+            if curr_date <= marr <= after_30days:
+                spouse_list.append("( " + check_husb + " " + check_wife + " )")
 
     return spouse_list
