ff0e077db10cdcbce508189d1192037ccba1277d
CelesteSakhile <celeste.sakile1994@gmail.com>
2017-11-14 21:36:35 -0500

    random changes

--- Sprint4/user_story_39.py
+++ Sprint4/user_story_39.py
@@ -1,10 +1,10 @@
 import GedcomClass
-import datetime
+from datetime import datetime
 from datetime import timedelta
 
 
 def upcomingAnniversaries(families,individuals):
-    curr_date = datetime.date.today()
+    curr_date = datetime.today()
     after_30days = curr_date + timedelta(days=30)
 
     spouse_list =[]
@@ -14,12 +14,12 @@ def upcomingAnniversaries(families,individuals):
         check_husb = individuals.get(check_family.husb).name
         check_wife = individuals.get(check_family.wife).name
         check_marr = check_family.marr
-        marr_day = int(check_marr.day)
-        marr_month = int(check_marr.month)
-        this_year = int(datetime.date.year)
-        this_marr = datetime.date(this_year,marr_month, marr_day)
+        #marr_day = int(check_marr.day)
+        #marr_month = int(check_marr.month)
+        #this_year = int(datetime.date.year)
+        #this_marr = datetime.date(this_year,marr_month, marr_day)
         if check_marr != 'NA':
-            if curr_date <= this_marr <= after_30days:
-                spouse_list.append("( "+ check_husb +" "+check_wife+" )")
+            if curr_date < check_marr < after_30days:
+                spouse_list.append(curr_date)
 
     return spouse_list
