39954840e645f7a4002892273b7c0ddfe7cbd0bb
Jiazhen Liu <ljzc18@gmail.com>
2017-11-14 21:47:44 -0500

    Fix us33

--- Sprint4/user_story_33.py
+++ Sprint4/user_story_33.py
@@ -1,22 +1,15 @@
-import GedcomClass
-from datetime import datetime, timedelta
+def recentOrphans(families, individuals):
+    orphans_list = []
 
-def recentOrphans(families,individuals):
-    curr_date = datetime.today()
-    before_30days = curr_date - timedelta(days=30)
-
-    orphans_list =[]
-
-    for family in families:
-        check_family = GedcomClass.get_family(family, families)
-        check_dad_alive = individuals.get(check_family.husb).alive
-        check_mom_alive= individuals.get(check_family.wife).alive
-        check_chil = check_family.chil_str
+    for family in families.values():
+        check_dad_alive = individuals.get(family.husb).alive
+        check_mom_alive = individuals.get(family.wife).alive
+        check_chil = family.chil
 
         if check_chil != 'NA':
             for chil in check_chil:
                 chil_age = individuals.get(chil).age
-                if(check_dad_alive == False and check_mom_alive == False and chil_age < 18):
+                if not check_dad_alive and not check_mom_alive and chil_age < 18:
                     orphans_list.append(chil)
 
     return orphans_list
